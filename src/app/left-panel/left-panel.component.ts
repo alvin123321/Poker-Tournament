bec858c1c3a3b10ace18568fcd82052b6feee64d
alvin <leealvin121@gmail.com>
2024-08-27 00:35:03 -0700

    wip

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { players } from '../player-list'; // Import the data array
+import { CommonModule } from '@angular/common'; // Import this to use ngFor
+import * as bootstrap from 'bootstrap';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-left-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, // Ensure this is imported
+  ],
   templateUrl: './left-panel.component.html',
-  styleUrl: './left-panel.component.css'
+  styleUrl: './left-panel.component.css',
 })
-export class LeftPanelComponent {
+export class LeftPanelComponent implements OnInit {
+  public playerList: any;
+  public activePlayer: any;
+  @Input() title: string = 'Confirm Action';
+  @Input() message: string = 'Are you sure you want to proceed?';
+  @Output() confirmed = new EventEmitter<void>();
+  constructor() {
+    this.playerList = players;
+    this.activePlayer = players.filter(
+      (player) => player.status === 'active'
+    ).length;
+    // Constructor logic here, typically dependency injection
+  }
 
+  ngOnInit(): void {
+    // Initialization logic here
+  }
+
+  confirm() {
+    this.confirmed.emit();
+    this.hideModal();
+  }
+
+  openConfirmModal() {
+    const modalElement = document.getElementById('confirmModal');
+    if (modalElement) {
+      const modalInstance = new bootstrap.Modal(modalElement);
+      modalInstance.show();
+    }
+  }
+
+  hideModal() {
+    const modalElement = document.getElementById('confirmModal');
+    if (modalElement) {
+      const modalInstance = bootstrap.Modal.getInstance(modalElement);
+      modalInstance ? modalInstance.hide() : console.log('modal hide error');
+    }
+  }
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,15 +1,117 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import this to use ngFor
 
 @Component({
   selector: 'app-timer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, // Ensure this is imported
+  ],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.css',
 })
 export class TimerComponent implements OnInit {
-  public minutes: number = 15;
+  public minutes: number = 1;
   public seconds: number = 0;
+  public currentLevel: number = 0;
+  public breakTime: number = 0;
+
+  public smallBlind = [
+    25,
+    50,
+    75,
+    100,
+    'break',
+    125,
+    150,
+    175,
+    200,
+    'break',
+    300,
+    400,
+    500,
+    700,
+    'break',
+    1000,
+    1500,
+    2000,
+    3000,
+    'break',
+    4000,
+    5000,
+    6000,
+    10000,
+    'break',
+    10000,
+    10000,
+    10000,
+    'break',
+    10000,
+    10000,
+    10000,
+    'break',
+    10000,
+    10000,
+    10000,
+    'break',
+    10000,
+    10000,
+    10000,
+    'break',
+  ];
+
+  public bigBlind = [
+    50,
+    100,
+    150,
+    200,
+    'break',
+    250,
+    300,
+    350,
+    400,
+    'break',
+    600,
+    800,
+    1000,
+    1400,
+    'break',
+    2000,
+    3000,
+    4000,
+    6000,
+    'break',
+    8000,
+    10000,
+    12000,
+    20000,
+    'break',
+    20000,
+    20000,
+    20000,
+    20000,
+    'break',
+    20000,
+    20000,
+    20000,
+    20000,
+    'break',
+    20000,
+    20000,
+    20000,
+    20000,
+    'break',
+    20000,
+    20000,
+    20000,
+    20000,
+    'break',
+    20000,
+    20000,
+    20000,
+    20000,
+  ];
+
   private intervalId: any;
   constructor() {
     // Constructor logic here, typically dependency injection
@@ -17,7 +119,6 @@ export class TimerComponent implements OnInit {
 
   ngOnInit(): void {
     this.startTimer();
-
     // Initialization logic here
   }
 
@@ -26,34 +127,23 @@ export class TimerComponent implements OnInit {
       // Decrease time by 1 second
       if (this.minutes >= 0) {
         this.seconds--;
-        console.log(this.seconds);
         if (this.seconds < 0) {
-          console.log('---------');
           if (this.minutes > 0) {
             this.minutes--;
-            this.seconds = 59;
+            this.seconds = 5;
           } else {
             {
-              this.minutes = 15;
+              if (this.smallBlind[this.currentLevel + 1] === 'break') {
+                this.breakTime++;
+              }
+              this.currentLevel++;
+
+              this.minutes = 1;
               this.seconds = 0;
-              clearInterval(this.intervalId);
             }
           }
         }
       }
-
-      // // Decrease time by 1 second
-      // if (totalTime > 0 && this.seconds > 0) {
-      //   this.seconds--;
-      //   console.log(this.seconds);
-      //   if (this.seconds < 0) {
-      //     this.minutes--;
-      //     this.seconds = 59;
-      //   }
-      // } else if ((this.seconds = 0 && this.minutes > 0)) {
-      // } else {
-      //   clearInterval(this.intervalId);
-      // }
-    }, 1000);
+    }, 500);
   }
 }
